433bc113768cdb30118f348df3e89ac94e0fc4a6
webcss12345 <88567879+webcss12345@users.noreply.github.com>
2021-08-07 08:27:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("1874957813:AAGLwnASBj6MEbaq3DL-puUsI9BGb4ic_iM")
+APP_ID = int(os.environ.get("4241128"))
+API_HASH = os.environ.get("1e0cd79fd4f95c64c9ee12b3c3c9ad2a")
 
 youtube_next_fetch = 0  # time in minute
 
